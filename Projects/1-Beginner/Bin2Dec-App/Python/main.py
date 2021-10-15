9bda880c29dab5a0d1f24062e505acc2593c3a0d
Cristiano Gonçalves <cprimbee@gmail.com>
2021-10-15 16:39:43 -0300

    feat: Bin2Dec - python - user can enter a variable number of binary digits

--- Projects/1-Beginner/Bin2Dec-App/Python/main.py
+++ Projects/1-Beginner/Bin2Dec-App/Python/main.py
@@ -1,15 +1,45 @@
-__initialize: bool = True
+from typing import List
+
+__initialize__: bool = True
 
 
 def main():
-    while __initialize:
-        binary_number: bin = input("Input an 8 digits binary number: ")
+    global __initialize__
+    while __initialize__:
+        answer = input("Multiple inputs? (y, N) ").capitalize()
+        answer = answer if answer != "" else "N"
 
-        if valid_binary(binary_number):
-            print(f"Typed binary number is {binary_number}. "
-                  f"Its relative decimal is {convert_bin_2_dec(binary_number)}")
+        if answer != "Y" and answer != "N":
+            __initialize__ = False
+            print("Invalid option!")
+
+        input_binary_number(answer)
 
         keep_going()
+
+
+def input_binary_number(multiple: str):
+    if multiple == "N":
+        single_input()
+    else:
+        multiple_input()
+
+
+def single_input():
+    binary_number: bin = input("Input an 8 digits binary number: ")
+
+    if valid_binary(binary_number):
+        print_result(binary_number, convert_bin_2_dec(binary_number))
+
+
+def multiple_input():
+    binary_numbers: List[bin] = input("Input a list of 8 digits binary numbers, separated by comma(,): ").split(",")
+
+    for binary_number in binary_numbers:
+        binary_number = binary_number.replace(" ", "")
+
+        if valid_binary(binary_number):
+            print_result(binary_number, convert_bin_2_dec(binary_number))
 
 
 def valid_binary(binary_number: bin) -> bool:
@@ -17,9 +47,9 @@ def valid_binary(binary_number: bin) -> bool:
         print(f"Max input's length is 8! You entered an binary with {binary_number} digits")
         return False
 
-    not_allowed_numbers: list = ['2', '3', '4', '5', '6', '7', '8', '9']
-    for not_allowed_number in not_allowed_numbers:
-        if not_allowed_number in binary_number:
+    allowed_numbers: list = ['0', '1']
+    for digit in binary_number:
+        if digit not in allowed_numbers:
             print("Input isn't a binary number")
             return False
 
@@ -30,23 +60,33 @@ def convert_bin_2_dec(binary_number: bin) -> str:
     counter: int = 0
     decimal_number: int = 0
     len_binary_number: int = len(binary_number) - 1
+
     while counter <= len_binary_number:
         index = len_binary_number - counter
+
         if int(binary_number[counter]) == 1:
             decimal_number += 2 ** index
+
         counter += 1
 
     return str(decimal_number)
 
 
+def print_result(binary_number: bin, decimal_number: str):
+    print(f"Typed binary number is {binary_number}. Its decimal equivalent is {decimal_number}")
+
+
 def keep_going():
-    global __initialize
+    global __initialize__
     must_continue = input("Continue ? (Y/n) ").capitalize()
+    must_continue = must_continue if must_continue != "" else "Y"
+
     if must_continue != "Y" and must_continue != "N":
-        __initialize = False
+        __initialize__ = False
         print('Invalid option!')
+
     if must_continue == "N":
-        __initialize = False
+        __initialize__ = False
 
 
 main()
